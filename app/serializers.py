50699374c63ac5974eac8b82ef9154598afca291
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2024-07-05 17:44:44 +0300

    fixed flake8

--- app/serializers.py
+++ app/serializers.py
@@ -1,6 +1,6 @@
 import json
 from abc import ABC, abstractmethod
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ElementTree
 
 from app.models import Book
 
@@ -21,9 +21,9 @@ class JsonBookSerializer(BookSerializer):
 class XmlBookSerializer(BookSerializer):
     @staticmethod
     def serialize(book: Book) -> str:
-        root = ET.Element("book")
-        title = ET.SubElement(root, "title")
+        root = ElementTree.Element("book")
+        title = ElementTree.SubElement(root, "title")
         title.text = book.title
-        content = ET.SubElement(root, "content")
+        content = ElementTree.SubElement(root, "content")
         content.text = book.content
-        return ET.tostring(root, encoding="unicode")
+        return ElementTree.tostring(root, encoding="unicode")
